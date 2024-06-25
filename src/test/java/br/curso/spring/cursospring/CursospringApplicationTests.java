4440b0a1c73976bd272512e71cb03950ed02262f
Lucas <lucas.rocha2021@outlook.com>
2024-06-24 23:24:39 -0300

    Criação do projeto

---
+++ src/test/java/br/curso/spring/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.curso.spring.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
